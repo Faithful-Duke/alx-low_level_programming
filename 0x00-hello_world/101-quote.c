715e92ccd5af9ea0711571df21c1e20ae617a808
Faithful-Duke <faithfulniemeyerduke@gmail.com>
2023-03-17 03:14:53 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A C program that prints to the standard an error message
- * Return 1 (Always)
+ * main - Entry point
+ * Return: Always 1
  */
+
 int main(void)
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-return: (1);
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }

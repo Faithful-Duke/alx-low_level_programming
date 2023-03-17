5c7874b0126e10ccd62fcf5da8ddb80bdfcdbe40
Faithful-Duke <faithfulniemeyerduke@gmail.com>
2023-03-16 21:16:51 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A C program that prints to the standard an error message
+ * Return 1 (Always)
+ */
+int main(void)
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return: (1);
+}

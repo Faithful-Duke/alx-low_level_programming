b28c451539efc71f1b7111801aabf2f49354669c
Faithful-Duke <faithfulniemeyerduke@gmail.com>
2023-03-21 20:05:18 -0700

    1-alphabet.c in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphabet lowercase
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
